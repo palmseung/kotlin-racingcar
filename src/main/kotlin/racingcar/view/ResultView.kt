e8b228548eed176608e4092e87ecb29a52b2fa68
palmseung <palmseung@gmail.com>
2021-03-03 09:24:13 +0900

    refactor(racing-car): make view object from class

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -6,28 +6,24 @@ private const val ONE_BLOCK = "-"
 private const val SEPARATOR = " | "
 private const val LINE_SEPARATOR = "=============================≠"
 
-class ResultView() {
-    companion object {
-        fun printResults(cars: List<Car>) {
-            cars.forEach { car ->
-                run {
-                    printName(car)
-                    printScore(car)
-                }
-            }
-            println(LINE_SEPARATOR)
+object ResultView {
+    fun printResults(cars: List<Car>) {
+        cars.forEach { car ->
+            printName(car)
+            printScore(car)
         }
+        println(LINE_SEPARATOR)
+    }
 
-        private fun printName(car: Car) {
-            print(car.name)
-            print(SEPARATOR)
-        }
+    private fun printName(car: Car) {
+        print(car.name)
+        print(SEPARATOR)
+    }
 
-        private fun printScore(car: Car) {
-            for (i in 1..car.score) {
-                print(ONE_BLOCK)
-            }
-            println()
+    private fun printScore(car: Car) {
+        for (i in 1..car.score) {
+            print(ONE_BLOCK)
         }
+        println()
     }
 }
--- src/main/kotlin/racingcar/view/WinnerView.kt
+++ src/main/kotlin/racingcar/view/WinnerView.kt
@@ -4,11 +4,9 @@ import racingcar.model.Car
 
 private const val STRING_JOINER = ", "
 
-class WinnerView() {
-    companion object {
-        fun printWinner(winners: List<Car>) {
-            val result = winners.joinToString(STRING_JOINER) { it.name }
-            println(">>>>> ${result}가 최종 우승했습니다. ")
-        }
+object WinnerView {
+    fun printWinner(winners: List<Car>) {
+        val result = winners.joinToString(STRING_JOINER) { it.name }
+        println(">>>>> ${result}가 최종 우승했습니다. ")
     }
 }
